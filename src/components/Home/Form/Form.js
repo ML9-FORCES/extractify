c6f8130715d385865189bd74011e1aa523dbfcae
Subin <subinsk284@gmail.com>
2022-01-02 14:08:32 +0530

    input box added

--- src/components/Home/Form/Form.js
+++ src/components/Home/Form/Form.js
@@ -5,6 +5,21 @@ import styles from './Form.module.css'
 function Form() {
     return (
         <div className={styles.container}>
+            <h1 className={styles.heading}>Select the type of file</h1>
+            <form className={styles.form}>
+                <label className={styles.label}>
+                    <input type="radio" name="radio" checked />
+                    <span>JPEG</span>
+                </label>
+                <label className={styles.label}>
+                    <input type="radio" name="radio" />
+                    <span>PNG</span>
+                </label>
+                <label className={styles.label}>
+                    <input type="radio" name="radio" />
+                    <span>PDF</span>
+                </label>
+            </form>
             <input type="file" name="file" id="file" />
         </div>
     )
