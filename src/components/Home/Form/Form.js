fb0ac0b13c415bf4e76661d771035b606dd5aad9
Subin <subinsk284@gmail.com>
2022-01-02 14:29:33 +0530

    error resolved of input

--- src/components/Home/Form/Form.js
+++ src/components/Home/Form/Form.js
@@ -1,22 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import styles from './Form.module.css'
 
 function Form() {
+    const [option, setOption] = useState("JPEG");
+
+    const optionHandler = (opt) => {
+        setOption(opt);
+    }
+
+    console.log(option)
     return (
         <div className={styles.container}>
             <h1 className={styles.heading}>Select the type of file</h1>
             <form className={styles.form}>
-                <label className={styles.label}>
-                    <input type="radio" name="radio" checked />
+                <label className={styles.label} >
+                    <input type="radio" name="radio" onClick={() => { optionHandler("JPEG") }} />
                     <span>JPEG</span>
                 </label>
-                <label className={styles.label}>
-                    <input type="radio" name="radio" />
+                <label className={styles.label} >
+                    <input type="radio" name="radio" onClick={() => { optionHandler("PNG") }} />
                     <span>PNG</span>
                 </label>
-                <label className={styles.label}>
-                    <input type="radio" name="radio" />
+                <label className={styles.label} >
+                    <input type="radio" name="radio" onClick={() => { optionHandler("PDF") }} />
                     <span>PDF</span>
                 </label>
             </form>
