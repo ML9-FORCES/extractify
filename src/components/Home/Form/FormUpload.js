7a692910d56fd5e3ce3fdbef2cba6e6d3104fba5
Subin <subinsk284@gmail.com>
2022-01-03 20:56:12 +0530

    added functionality of pdf upload successfully

--- src/components/Home/Form/FormUpload.js
+++ src/components/Home/Form/FormUpload.js
@@ -1,14 +1,31 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import axios from 'axios'
+import FormUploader from './FormUploader';
 import styles from './FormUpload.module.css'
 
 function FormUpload(props) {
+    const [selectedFile, setSelectedFile] = useState(null);
+
+    const submitForm = (e) => {
+        e.preventDefault()
+        const formData = new FormData();
+        formData.append("pdf_name", selectedFile);
+
+        axios
+            .post('http://127.0.0.1:8000/converter/pdftoimg/', formData)
+            .then((res) => {
+                alert("File Upload success");
+            })
+            .catch((err) => alert("File Upload Error"));
+    };
 
     return (
         <form id={styles.fileUploadForm} className={`${styles.uploader} ${props.option ? '' : styles.disable}`}>
-            <input id='fileUpload' type="file" name="fileUpload" accept={`.${props.option}`} disabled={props.option ? false : true} />
+            {/* <input
+                value={selectedFile}
+                onChange={(e) => setSelectedFile(e.target.files[0])} id='fileUpload' type="file" name="fileUpload" accept={`.${props.option}`} disabled={props.option ? false : true} /> */}
 
-            <label htmlFor="fileUpload" id="file-drag">
+            <label htmlFor="fileUpload" id="file-drag" >
                 <img id={styles.fileImage} src="#" alt="Preview" className={styles.hidden} />
                 <div id={styles.start}>
                     <i className={`${styles.fa} ${styles.faDownload}`} aria-hidden="true"></i>
@@ -16,6 +33,14 @@ function FormUpload(props) {
                     <div id={styles.notimage} className={styles.hidden}>Please select an image</div>
                     <span id={styles.fileUploadBtn} className={`${styles.btn} ${styles.btnPrimary}`}>Select a file</span>
                 </div>
+                <FormUploader
+                    id='fileUpload'
+                    accept={`.${props.option}`} disabled={props.option ? false : true}
+                    onFileSelectSuccess={(file) => setSelectedFile(file)}
+                    onFileSelectError={({ error }) => alert(error)}
+                />
+                <button className={`${styles.btn} ${styles.btnPrimary}`} onClick={(e) => submitForm(e)}>Submit</button>
+
                 <div id={styles.response} className={styles.hidden}>
                     <div id={styles.messages}></div>
                     <progress className={styles.progress} id={styles.fileProgress} value="0">
---
+++ src/components/Home/Form/FormUploader.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function FormUploader({ id, name, accept, disabled, onFileSelectError, onFileSelectSuccess }) {
+    const handleFileInput = (e) => {
+        // handle validations
+        const file = e.target.files[0];
+        if (file.size > 10000000)
+            onFileSelectError({ error: "File size cannot exceed more than 10MB" });
+        else onFileSelectSuccess(file);
+    }
+    return (
+        <div className="file-uploader">
+            <input type="file" id={id} disabled={disabled} accept={accept} name={name} onChange={handleFileInput} />
+        </div>
+    )
+}
+
+export default FormUploader
